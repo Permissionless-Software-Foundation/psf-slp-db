c6ab053684760d358a37d19a4984f3dacc966d96
Chris Troutner <chris.troutner@gmail.com>
2025-10-08 18:55:38 -0700

    Fixing getAllTxData.js script

--- util/index/getAllTxData.js
+++ util/index/getAllTxData.js
@@ -2,7 +2,13 @@
   Utility tool to retrieve all token keys in the token DB.
 */
 
-const level = require('level')
+// const level = require('level')
+import level from 'level'
+
+// Hack to get __dirname back.
+// https://blog.logrocket.com/alternatives-dirname-node-js-es-modules/
+import * as url from 'url'
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
 const txDb = level(`${__dirname.toString()}/../../leveldb/current/txs`, {
   valueEncoding: 'json'
