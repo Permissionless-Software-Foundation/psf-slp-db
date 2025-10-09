eaed45fd881f5d9be7aef3dd482d6fa5c43bec87
Chris Troutner <chris.troutner@gmail.com>
2025-10-08 17:41:17 -0700

    Refactoring util libs

--- util/index/getAllTokens.js
+++ util/index/getAllTokens.js
@@ -2,30 +2,32 @@
   Utility tool to retrieve all token keys in the token DB.
 */
 
-// Public npm libraries.
-import { Level } from 'level'
+// const level = require('level')
+import level from 'level'
 
 // Hack to get __dirname back.
 // https://blog.logrocket.com/alternatives-dirname-node-js-es-modules/
 import * as url from 'url'
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
-const tokenDb = new Level(`${__dirname.toString()}/../../leveldb/current/tokens`, {
+
+const tokenDb = level(`${__dirname.toString()}/../../leveldb/current/tokens`, {
   valueEncoding: 'json'
 })
 
 async function getTokens () {
   try {
-    // Level v8 uses async iterators instead of streams
-    for await (const [key, value] of tokenDb.iterator()) {
-      // console.log(key, ' = ', value)
+    const stream = tokenDb.createReadStream()
 
-      if (value.totalBurned !== '0' && value.totalBurned !== value.totalMinted) {
-        value.totalTxs = value.txs.length
-        value.txs = []
-        console.log(key, ' = ', value)
+    stream.on('data', function (data) {
+      // console.log(data.key, ' = ', data.value)
+
+      if (data.value.totalBurned !== '0' && data.value.totalBurned !== data.value.totalMinted) {
+        data.value.totalTxs = data.value.txs.length
+        data.value.txs = []
+        console.log(data.key, ' = ', data.value)
       }
-    }
+    })
   } catch (err) {
     console.error(err)
   }
--- util/index/getAllUtxos.js
+++ util/index/getAllUtxos.js
@@ -2,21 +2,27 @@
   Utility tool to retrieve all token keys in the token DB.
 */
 
-const level = require('level')
+// const level = require('level')
+import level from 'level'
 
-const tokenDb = level(`${__dirname.toString()}/../../leveldb/current/utxos`, {
+// Hack to get __dirname back.
+// https://blog.logrocket.com/alternatives-dirname-node-js-es-modules/
+import * as url from 'url'
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+
+const utxoDb = level(`${__dirname.toString()}/../../leveldb/current/utxos`, {
   valueEncoding: 'json'
 })
 
-async function getTokens () {
+async function getUtxos () {
   try {
-    const stream = tokenDb.createReadStream()
+    const stream = utxoDb.createReadStream()
 
     stream.on('data', function (data) {
       console.log(data.key, ' = ', data.value)
     })
   } catch (err) {
-    console.error(err)
+    console.error(err.message)
   }
 }
-getTokens()
+getUtxos()
